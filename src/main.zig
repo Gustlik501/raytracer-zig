5c79ad14f81a75be01141f521ad362f72bf6e1c9
Gustlik501 <sevcnikar.gregor2@gmail.com>
2025-02-14 17:51:11 +0100

    Added some abstractions... And a second sphere :strong:

--- src/main.zig
+++ src/main.zig
@@ -33,6 +33,27 @@ pub fn solveQuadratic(a: f32, b: f32, c: f32) ?[2]f32 {
     return solutions;
 }
 
+pub const Sphere = struct {
+    position: @Vector(3, f32),
+    radius: f32,
+    color: @Vector(3, u8),
+};
+
+pub const Ray = struct {
+    origin: @Vector(3, f32),
+    direction: @Vector(3, f32),
+
+    pub fn getSphereIntersections(self: *const Ray, sphere: Sphere) ?[2]f32 {
+        const distance_to_origin = self.origin - sphere.position;
+
+        const a = dotProduct(self.direction, self.direction); //Always 1???
+        const b = 2 * dotProduct(self.direction, distance_to_origin);
+        const c = dotProduct(distance_to_origin, distance_to_origin) - (sphere.radius * sphere.radius);
+
+        return solveQuadratic(a, b, c);
+    }
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -42,8 +63,12 @@ pub fn main() !void {
     var img = try Image.create(640, 480, allocator);
     defer allocator.free(img.pixels);
 
-    const circle_radius = 50;
-    const circle_position = @Vector(3, f32){ 0, 0, 0 };
+    var spheres = std.ArrayList(Sphere).init(allocator);
+    defer spheres.deinit();
+
+    _ = try spheres.append(Sphere{ .position = .{ 0, 0, -100 }, .radius = 50, .color = .{ 255, 100, 255 } });
+    _ = try spheres.append(Sphere{ .position = .{ 0, -50, -50 }, .radius = 50, .color = .{ 0, 255, 255 } });
+
     const camera_position = @Vector(3, f32){ 0, 0, -200 };
 
     const view_direction = @Vector(3, f32){ 0, 0, 1 };
@@ -52,7 +77,10 @@ pub fn main() !void {
     const screen_center = camera_position + (view_direction * focal_distance);
     const screen_width_half: f32 = @floatFromInt(img.width / 2);
     const screen_height_half: f32 = @floatFromInt(img.height / 2);
-    const distance_to_origin = camera_position - circle_position;
+
+    const closestIntersections = try allocator.alloc(f32, @as(u32, img.width) * img.height);
+    @memset(closestIntersections, std.math.floatMax(f32));
+    defer allocator.free(closestIntersections);
 
     for (0..img.width) |x| {
         for (0..img.height) |y| {
@@ -63,16 +91,26 @@ pub fn main() !void {
             const signed_distance_from_camera = current_pixel - camera_position;
             const ray_direction = normalizeVector(signed_distance_from_camera);
 
-            const a = dotProduct(ray_direction, ray_direction);
-            const b = 2 * dotProduct(ray_direction, distance_to_origin);
-            const c = dotProduct(distance_to_origin, distance_to_origin) - (circle_radius * circle_radius);
+            const ray = Ray{ .origin = camera_position, .direction = ray_direction };
 
-            const intersections = solveQuadratic(a, b, c);
-            if (intersections != null) {
-                //print("Checking pixel {},{}: ray_direction: {d} - distance_from_camera: {d}\n", .{x,y,ray_direction, signed_distance_from_camera});
-                //print("Found intersection at pixel: {},{}\n", .{ x, y });
-                try img.setPixel(@intCast(x), @intCast(y), .{ 255, 255, 255 });
+            for (spheres.items) |sphere| {
+                const intersections = ray.getSphereIntersections(sphere);
+                if (intersections != null) {
+                    const index = y * img.width + x;
+                    const first_intersection = intersections.?[0];
+                    if (first_intersection < closestIntersections[index]) {
+                        try img.setPixel(@intCast(x), @intCast(y), sphere.color);
+                        closestIntersections[index] = first_intersection;
+                    }
+                }
             }
+
+            // const intersections = ray.getSphereIntersections(sphere1);
+            // if (intersections != null) {
+            //     //print("Checking pixel {},{}: ray_direction: {d} - distance_from_camera: {d}\n", .{x,y,ray_direction, signed_distance_from_camera});
+            //     //print("Found intersection at pixel: {},{} -- Intersection points: {d},{d}\n", .{ x, y , intersections.?[0], intersections.?[1]});
+            //     try img.setPixel(@intCast(x), @intCast(y), sphere1.color);
+            // }
         }
     }
 
